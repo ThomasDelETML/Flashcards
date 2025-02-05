bb344a65912658160a205ae0b2886fc1a94c5c80
ThomasDelETML <po51oro@eduvaud.ch>
2025-02-05 15:25:42 +0100

    code(routes.ts): Si on l'applique, ce commit va ajouter une bar de navigation fonctionnel
    [60min][DONE]

--- code/flashcards/start/routes.ts
+++ code/flashcards/start/routes.ts
@@ -9,4 +9,22 @@
 
 import router from '@adonisjs/core/services/router'
 
-router.on('/').render('pages/home')
+// router.on('/').render('pages/home')
+
+router
+  .get('/', async ({ view }) => {
+    return view.render('pages/home')
+  })
+  .as('home')
+
+router
+  .get('/flashcards', async ({ view }) => {
+    return view.render('pages/flashcards')
+  })
+  .as('flashcards')
+
+router
+  .get('/login', async ({ view }) => {
+    return view.render('pages/login')
+  })
+  .as('login')
